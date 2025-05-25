aa1902c4d8c79789b22a6a4449a3e4fd956417a4
champiao <rafael.leite.assis63@gmail.com>
2025-05-25 20:07:59 -0300

    animations

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideServerRendering()
+    provideServerRendering(),
+    provideAnimations()
   ]
 };
